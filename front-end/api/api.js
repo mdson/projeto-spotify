5b0d69df4c57e761773be27c09b346083ad81f10
Madson <madsonsmitt@gmail.com>
2025-11-25 18:30:23 -0300

    Feat: Conectando com backend de produção

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL ='http://localhost:3001';
+const URL ='https://spotify-backend-e1av.onrender.com';
 
 const resArtists = await axios.get(`${URL}/artists`);
 const resSongs = await axios.get(`${URL}/songs`);
